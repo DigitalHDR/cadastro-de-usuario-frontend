a25cdb3f289f61ea15d4bd00bf623ab44cb36ba8
DigitalHDR <digitalextremeultra@gmail.com>
2021-03-13 21:48:15 -0300

    function no usercrud

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Main from '../template/Main'
-
+import axios from 'axios'
 
 const headerProps = {
     icon: 'users',
@@ -8,9 +8,42 @@ const headerProps = {
     subtitle: 'Cadastro de usuário: Incluir, Listar, Alterar e Excluir.'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
 
 // Lembrete apagar a class e fazer com function
 export default class UserCrud extends Component {
+
+    state = { ...initialState }
+
+    clear() {
+        this.setState({ user: initialState.user })
+        // isso faz limpar apenas o usuário
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        //se usuário id for verdadeiro ou usuário id falso
+        //put verdadeiro vai ser alterado o usuário
+        //post vai incluir um usuário
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+        .then(resp => {
+            const list = this.getUpdatedList(resp.data)
+            this.setState({ user: initialState.user, list })
+        })
+    }
+
+    getUpdatedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
     render() {
         return (
             <Main {...headerProps} >
